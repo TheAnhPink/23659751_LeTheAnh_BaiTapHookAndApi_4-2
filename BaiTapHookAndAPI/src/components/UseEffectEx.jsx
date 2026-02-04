3f6379843d9a51f86b5bb9574e87761d6296aeef
TheAnhPink <23659751.anh@student.iuh.edu.vn>
2026-02-04 16:45:08 +0700

    UseEffect

--- BaiTapHookAndAPI/src/components/UseEffectEx.jsx
+++ BaiTapHookAndAPI/src/components/UseEffectEx.jsx
@@ -3,9 +3,9 @@ import { useEffect} from 'react'
 
 
 function UseEffectEx() {
-  useEffect(() => {
+  useEffect(()=> {
     alert("Chào thầy ạ!");
-  }, []); // [] trống nghĩa là chỉ hiện thông báo đúng 1 lần duy nhất khi mở web
+  },[]);//[] trống nghĩa là chỉ hiện thông báo đúng 1 lần duy nhất khi mở web
 
   return <h3>Load lại trang để xem thông báo</h3>;
 }
